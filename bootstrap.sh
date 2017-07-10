9af1152d4c561d26a5983807138a67157162e7b5
Diego Caravana <diego@caravana.to>
2017-07-10 23:01:16 +0100

    - reviewed/rewritted README.md

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-PROJECT=interview_tangent
-ENV_BASE=~/envs
+PROJECT=interview_restapi
+ENV_BASE=~/.virtualenvs
 ENV=$ENV_BASE/$PROJECT-python
 
 if [[ ! -d $ENV ]]; then
     mkdir -p $ENV
     virtualenv --no-site-packages $ENV
+    $ENV/bin/pip install --upgrade pip #else cryptography install fails
 fi
 
 $ENV/bin/pip install -r requirements.txt
 
+source $ENV/bin/activate
+
 python setup.py develop
+
+echo
+echo Now do:
+echo source $ENV/bin/activate
+echo ./manage.py migrate
+echo ./manage.py createsuperuser --username admin --email admin@nowhere.no
+echo [enter \"admin001\" as password]
+echo ./manage.py runserver
